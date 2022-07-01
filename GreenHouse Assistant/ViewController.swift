f32f66e6d72fc33bbdb490859cacccebf876d4e4
Sofia Gonzalez <sofiiaglz@hotmail.com>
2022-06-30 23:12:52 -0500

    Initial Commit

---
+++ GreenHouse Assistant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GreenHouse Assistant
+//
+//  Created by Mac19 on 30/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
