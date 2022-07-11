152dfb451c1270cfdd23bb9330fa48c0fabf771a
Sofía Gonzalez <sofiiaglz@hotmail.com>
2022-07-11 12:08:31 -0500

    Subo nav con tab bar

--- GreenHouse Assistant/ViewController.swift
+++ GreenHouse Assistant/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passField: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
+    @IBAction func IniciarSesion(_ sender: Any) {
+      /*  let fc: String?=(emailField.text!)
+        let fp: String?=(passField.text!)
+        if let fc2 = fc, let fp2 = fp{
+            
+        }else if fc != nil{
+            
+        }else{
+            
+        }*/
+    }
 }
 
